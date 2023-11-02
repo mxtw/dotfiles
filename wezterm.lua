b5055f1009d8502eac28ee9061f0608520c18208
max <30047099+mxtw@users.noreply.github.com>
2023-11-02 20:53:08 +0100

    custom tab colors

--- wezterm.lua
+++ wezterm.lua
@@ -2,8 +2,23 @@ local wezterm = require("wezterm")
 
 require("bar")
 
+local custom = wezterm.color.get_builtin_schemes()["Catppuccin Mocha"]
+
 local config = {
     color_scheme = "Catppuccin Mocha",
+    colors = {
+        tab_bar = {
+            background = "rgba(30, 30, 46, 0.92)", -- "#1e1e2e"
+            active_tab = {
+                bg_color = "#b4befe",
+                fg_color = "#1e1e2e",
+            },
+            inactive_tab = {
+                bg_color = "rgba(30, 30, 46, 0.92)", -- #1e1e2e",
+                fg_color = "#b4befe",
+            }
+        }
+    },
     font = wezterm.font("Hack"),
     font_size = 17.0,
     window_background_opacity = 0.92,
@@ -12,6 +27,7 @@ local config = {
     use_fancy_tab_bar = false,
     tab_bar_at_bottom = false,
     warn_about_missing_glyphs = false,
+    show_new_tab_button_in_tab_bar = false,
     keys = {
 
         -- splits
