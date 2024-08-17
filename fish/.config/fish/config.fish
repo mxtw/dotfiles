2593b5e07a4a265cbe3e2b455c02627508e5fc45
max <30047099+mxtw@users.noreply.github.com>
2024-08-17 11:07:55 +0200

    feat(fish): add fzf options

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,11 @@
 # global vars
 set -gx EDITOR nvim
+set -gx FZF_DEFAULT_OPTS "\
+--color=bg+:#313244,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
 
 # path
 fish_add_path $HOME/.local/bin
